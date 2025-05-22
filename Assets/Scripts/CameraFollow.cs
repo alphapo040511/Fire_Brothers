d4c75beb04d5e32743f195a458414b359f7f3cc0
alphapo040511 <alphapo05@gmail.com>
2025-05-22 18:03:02 +0900

    [Chore] 카메라 타겟 설정 변경

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -14,24 +14,15 @@ public class CameraFollow : MonoBehaviour
 {
     public CameraStats cameraStats;                             //카메라 상태
 
-    public Transform[] targets;                                 //소방차의 transform
-
-    //Start에서 차량 오브젝트 검색 후 중점 구해도 괜찮을 듯
-    public bool hasMultipleTargets = false;                     //따라가야 하는 타겟이 여러개인지
+    public Transform targets;
 
     public Vector3 cameraOffset = new Vector3(4, 18, 0);       //카메라의 추적시 위치
 
     void Start()
     {
-        VehicleMove[] temp = FindObjectsOfType<VehicleMove>();
-
-        if (temp.Length <= 0) return;
-
-        targets = new Transform[temp.Length];
-
-        for(int i = 0; i < temp.Length; i++)
+        if(targets == null && PlayableObjectsManager.Instance != null)
         {
-            targets[i] = temp[i].transform;
+            targets = PlayableObjectsManager.Instance.transform;
         }
     }
 
@@ -48,29 +39,17 @@ public class CameraFollow : MonoBehaviour
 
     private void FollowVehicle()
     {
-        if(targets.Length <= 0)
+        if (targets == null)
         {
-            Debug.LogWarning("차량을 찾을 수 없습니다.");
+            if (PlayableObjectsManager.Instance != null)
+            {
+                targets = PlayableObjectsManager.Instance.transform;
+            }
             return;
         }
 
-        Vector3 targetPos = TargetPosition();
-
-        transform.position = targetPos + cameraOffset;
-        transform.LookAt(targetPos);
-    }
-
-    private Vector3 TargetPosition()
-    {
-        int count = 0;
-        Vector3 cneter = Vector3.zero;
-        for (int i = 0; i < targets.Length; i++)
-        {
-            count++;
-            cneter += targets[i].transform.position;
-        }
-
-        return cneter / count;
+        transform.position = targets.position + cameraOffset;
+        transform.LookAt(targets.position);
     }
 
 }
