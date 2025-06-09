e868712406713edb47108362af65ada8e55142da
alphapo040511 <alphapo05@gmail.com>
2025-06-09 15:19:57 +0900

    [Feat] 현재 디바이스 종류 표시 기능 추가

--- Assets/Scripts/UI/PauseUI.cs
+++ Assets/Scripts/UI/PauseUI.cs
@@ -1,10 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseUI : MonoBehaviour
 {
+    public Image[] deviceImage;
+    public Sprite keyboardImage;
+    public Sprite controllerImage;
+
+    private void OnEnable()
+    {
+        if (InputDeviceManager.Instance == null) return;
+
+        foreach (var device in InputDeviceManager.Instance.InputDevices)
+        {
+            if (device.Key < 2)
+            {
+                if (device.Value is Keyboard keyboard)
+                {
+                    deviceImage[device.Key].sprite = keyboardImage;
+                }
+                else if (device.Value is Gamepad gamepad)
+                {
+                    deviceImage[device.Key].sprite = controllerImage;
+                }
+                else
+                {
+                    deviceImage[device.Key].sprite = null;
+                }
+            }
+        }
+    }
+
     public void ShowControllerSetting()
     {
         UIManager.Instance.ShowScreen(ScreenType.ControllerSet);
