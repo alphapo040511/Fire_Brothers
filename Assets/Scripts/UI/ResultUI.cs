facdf45167126f87c45b58a9066f1064d16e9c59
alphapo040511 <alphapo05@gmail.com>
2025-06-12 20:35:30 +0900

    [Fix] 종료 화면 애니메이션이 안나오던 문제 수정

--- Assets/Scripts/UI/ResultUI.cs
+++ Assets/Scripts/UI/ResultUI.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,9 +7,15 @@ using UnityEngine.UI;
 public class ResultUI : MonoBehaviour
 {
     public GameObject ResultCanvas;
+    public Button quitButton;
 
     public TextMeshProUGUI scoreText;
     public Image[] stars = new Image[3];
+
+    public Animator player_1;
+    public Animator player_2;
+
+    private bool isWorking = false;
 
     private void OnEnable()
     {
@@ -24,32 +29,48 @@ public class ResultUI : MonoBehaviour
 
     public void ChangeGameState(GameState state)
     {
-        if(state == GameState.GameOver)
+        if(state == GameState.GameOver && !isWorking)
         {
-            ShowScore();
+            StartCoroutine(ShowScore());
         }
     }
 
-    public void ShowScore()
+    public IEnumerator ShowScore()
     {
+        isWorking = true;
+
         ResultCanvas.SetActive(true);
+
+        bool isClear = StageManager.Instance.IsStageClaer(GameManager.Instance.currentStageIndex);
+        string trigger = isClear ? "Success" : "Fail";
+
+        player_1.SetTrigger(trigger);
+        player_2.SetTrigger(trigger);
 
         int score = StageStatsManager.Instance.currentScore;
 
-        int starCount = StageManager.Instance.CalculateStars(score, StageManager.Instance.GetStarThreshold());
+        int[] starThreshold = StageManager.Instance.GetStarThreshold();
 
-        if(scoreText != null)
+        if (scoreText != null)
         {
-            scoreText.text = $"Scroe : {score}";
-        }
-
-        for(int i = 0; i < starCount; i++)
-        {
-            if (stars[i] != null)
+            for (int i = 0; i <= score; i++)
             {
-                stars[i].gameObject.SetActive(true);
+                scoreText.text = $"Scroe : {i}";
+                for (int j = 0; j < starThreshold.Length; j++)
+                {
+                    if (stars[j] != null && i >= starThreshold[j])
+                    {
+                        stars[j].gameObject.SetActive(true);
+                    }
+                }
+                yield return null;
             }
         }
+
+        yield return new WaitForSecondsRealtime(1);
+
+        quitButton.interactable = true;
+        isWorking = false;
     }
 
     public void Quit()
